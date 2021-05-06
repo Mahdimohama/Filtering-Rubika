00a7727eda1927380ec908edf862c22e6f9be9a5
MOJTABA-CASSIFER <79422726+C4ssif3r@users.noreply.github.com>
2021-05-06 12:36:22 +0430

    Filtering-Rubika.py

--- Filtering-Rubika.py
+++ Filtering-Rubika.py
@@ -50,7 +50,7 @@ if update == "1":
 code = (input(" type : 2 for get code :  "))
 time.sleep(3.0)
 print(f"{red} ")
-if code == "2": print ("code is    yttk_155770   for Update type 1 -")
+if code == "2": print ("code is    yttk_15570   for Update type 1 -")
 time.sleep(3.0)
 print(f"{blue}" )
 print (" Baraye Estefade Az code dar kanal rubika' man ozv shin-!🌟 -! ")
