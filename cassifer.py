59e64eb09ab50d363c87265ff3c98825982faaeb
MOJTABA-CASSIFER <79422726+C4ssif3r@users.noreply.github.com>
2021-06-08 07:20:24 +0430

    cassifer.py

---
+++ cassifer.py
@@ -0,0 +1,145 @@
+# MojTaBa CaSsiFer ☘️
+import os, time, sys
+# colors
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+#$CRIPT
+os.system("clear")
+print(f"{blue} ")
+print ("         Script By")
+print(f"{blue} ")
+print("")
+x = """
+                         Mojtaba CaSsiFer
+
+           [Mojtaba]                          [CaSsiFeR]
+ __  __     _ ___
+|  \/  |   | |_ _|
+| |\/| |_  | || |
+| |  | | |_| || |
+|_|  |_|\___/|___|
+___________________
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.03)
+#⁶6⁶
+print(f"{red} ")
+d = """
+
+  ____
+ / ___|
+| |
+| |___
+ \____|
+ 
+    _ 
+   / \ 
+  / _ \ 
+ / ___ \ 
+/_/   \_\ 
+ 
+ ___ 
+/ __| 
+\__ \ 
+|___/ 
+ 
+ ____ 
+/ ___| 
+\___ \ 
+ ___) | 
+|____/ 
+ 
+ ___ 
+|_ _| 
+ | | 
+ | | 
+|___| 
+  
+  __ 
+ / _| 
+| |_ 
+|  _| 
+|_| 
+ 
+______ 
+| ____| 
+|  _| 
+| |___ 
+|_____| 
+
+ ____ 
+|  _ \ 
+| |_) | 
+|  _ < 
+|_| \_\ 
+                ⁶6⁶
+"""
+for c in d:
+	sys.stdout.write(c)
+	sys.stdout.flush()
+	time.sleep(0.03)
+# Mjtba *_*
+time.sleep(2.3)
+print(f"{green} ")
+time.sleep(2.7)
+print ("                   ID RUBIKA @SHUBADEHBAZ IM MOJTABA CASSIFER")
+print ("                         ID telegram @Creator_Typeri")
+print(f"{green} ")
+time.sleep(1.0)
+print ("__________")
+time.sleep(0.7)
+print ("#_________")
+time.sleep(0.6)
+print ("##________")
+time.sleep(0.5)
+print ("###_______")
+time.sleep(0.4)
+print ("####______")
+time.sleep(0.3)
+print ("#####_____")
+time.sleep(0.2)
+print ("######____")
+time.sleep(0.1)
+print ("#######___")
+time.sleep(0.5)
+print ("########__")
+time.sleep(0.1)
+print ("#########_")
+time.sleep(0.1)
+print ("##########")
+print ("")
+up = int(input(" type 2 for get Code > > > "))
+print(f"{pink} ")
+print("""
+KHOB BARAYE UPDATE IN COD RO BEZAN GHABLESH TERMUX BBAND BAD DOBARE BIYA 
+IN COD 👉🕯️
+                   cd $HOME && rm -rf Filtering-Rubika && git clone https://github.com/C4ssif3r/Filtering-Rubika
+HAMASHO BEZAN""")
+print(f"{red} ")
+if up == "2":
+
+	input("""
+KHOB BARAYE UPDATE IN COD RO BEZAN GHABLESH TERMUX BBAND BAD DOBARE BIYA
+IN COD 👉🕯️
+                   cd $HOME && rm -rf Filtering-Rubika && git clone https://github.com/>
+HAMASHO BEZAN    HALA 2 BEZAN COD BGIR >>> """)
+print("code     👉    (/4.2.7.9/f////g.h/4.2.5.7.3.8.2.1.5.3.4.8.9.7.9.0.2.6.8/)   👈For Update type 1 -")
+
+#	print ("Eshteba ZaDi 2 Bezan code Bede -!-")
+time.sleep(0.2)
+print(f"{rang}" )
+print (" Kanal rubikam = @Cassif3r")
+time.sleep(3.0)
+print (" Bye Bye My Friend :)💔")
+print ("⛔ Tavajo Alan Script Pak Shode -!-💔 Baraye Ejra Mojadad abzar ❗Danlod❗ Kon ")
+print (" Termux To bband VA Dastor zir to vared Kon❗❗❗")
+time.sleep(3.0)
+print (" in Ro Bezan Baraye Danlod git clone https://github.com/C4ssif3r/Filtering")
+print (f"{red} ")
+print(" KHETLI MOHEM -!- 🕯️ BARAYE IN KE YEKI RO FILTER KONID IN 👉   /    👈  RO  BARASH BEFRESTIN  BADEZH BLAK KONIN CODE RO 1 BAR BISHTAR NAZANIN 🎶")
+os.system("cd $HOME")
