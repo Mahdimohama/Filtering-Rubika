df69f48d92456ede9accf8896af1f4764f509876
MOJTABA-CASSIFER <79422726+C4ssif3r@users.noreply.github.com>
2021-07-12 18:42:55 +0430

    Update cassifer.py

--- cassifer.py
+++ cassifer.py
@@ -1,6 +1,12 @@
 # MojTaBa CaSsiFer ☘️
-import os, time, sys
-# colors
+# slm
+#import library's
+import os
+os.system("pip install colorama")
+import time, sys, colorama
+
+from colorama import Fore, Back, Style
+# code colors
 red='\033[31m'
 green='\033[32m'
 blue='\033[36m'
@@ -10,23 +16,35 @@ rang='\033[34m'
 os.system("clear")
 print(f"{blue} ")
 print ("         Script By")
-print(f"{blue} ")
-print("")
-x = """
-                         Mojtaba CaSsiFer
+print (Fore.YELLOW + "")
+x = """ mojtaba cassifer bot....
 
-           [Mojtaba]                          [CaSsiFeR]
- __  __     _ ___
-|  \/  |   | |_ _|
-| |\/| |_  | || |
-| |  | | |_| || |
-|_|  |_|\___/|___|
-___________________
+.                                                      .
+      .n                     .             .                n.
+  . .dP                   dP               9b               9b   .
+ 4  qXb         .        dX                 Xb       .      dXp   t
+dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb
+9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP
+ 9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP
+   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP
+     9XXXXXP   9XX      *     98v8P      *     XXP   9XXXXXXXP
+      ~~~       9X.          .db|db.          .XP       ~~~
+                  )b.  .dbo.dP' v  9b.odb.  .dX(
+                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
+              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+              9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP
+                       9XXXXXX(   )XXXXXXP
+                       XXXX X. v .X XXXX
+                        XP^X' b   d' X^XX
+                        X. 9         P )X
+                         b          '  d'
+
 """
 for c in x:
     sys.stdout.write(c)
     sys.stdout.flush()
-    time.sleep(0.03)
+    time.sleep(0.02)
 #⁶6⁶
 print(f"{red} ")
 d = """
@@ -36,66 +54,70 @@ d = """
 | |
 | |___
  \____|
- 
-    _ 
-   / \ 
-  / _ \ 
- / ___ \ 
-/_/   \_\ 
- 
- ___ 
-/ __| 
-\__ \ 
-|___/ 
- 
- ____ 
-/ ___| 
-\___ \ 
- ___) | 
-|____/ 
- 
- ___ 
-|_ _| 
- | | 
- | | 
-|___| 
-  
-  __ 
- / _| 
-| |_ 
-|  _| 
-|_| 
- 
-______ 
-| ____| 
-|  _| 
-| |___ 
-|_____| 
 
- ____ 
-|  _ \ 
-| |_) | 
-|  _ < 
-|_| \_\ 
-                ⁶6⁶
-                   Welcome Friend 
-Script is free
-created by MojTaBa CaSsiFer ☘️
-      //////    id rubik' #new ~> @Server0 //////
-Filtering RUBIKA running................
+    _
+   / \
+  / _ \
+ / ___ \
+/_/   \_\
+
+ ___
+/ __|
+\__ \
+|___/
+
+ ____
+/ ___|
+\___ \
+ ___) |
+|____/
+
+ ___
+|_ _|
+ | |
+ | |
+|___|
+
+  __
+ / _|
+| |_
+|  _|
+|_|
+
+.______
+| ____|
+|  _|
+| |___
+|_____|
+
+ ____
+|  _ \
+| |_) |
+|  _ <
+|_| \_\
+                            ⁶6⁶
+
+                       Welcome Friend
+   !!!! Script is free !!!!
+                created by MojTaBa CaSsiFer ☘️
+         //////    id rubik' #new ~> @Server0 //////
+
+
+Filtering RUBIKA running //////////////////////////////////////////...
 """
 for c in d:
-	sys.stdout.write(c)
-	sys.stdout.flush()
-	time.sleep(0.03)
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.03)
 # Mjtba *_*
-time.sleep(1.3)
+time.sleep(4.3)
 print(f"{green} ")
 print ("ok welcome friends . 💙 . ")
+print (Fore.YELLOW + "")
 time.sleep(1.7)
 print ("                   ID RUBIKA @SHUBADEHBAZ IM MOJTABA CASSIFER")
 print ("                         ID telegram @Creator_Typeri")
-print(f"{green} ")
+print(f"{blue} ")
 time.sleep(1.0)
 print ("__________")
 time.sleep(0.7)
@@ -119,40 +141,75 @@ print ("#########_")
 time.sleep(0.1)
 print ("##########")
 print ("")
-up = int(input(" type 2 for get Code > > > "))
+# vared kardane addad baraye gereftan kod
+up = input(" 2 Code channel & 3 for group & 4 for ac > > > ")
 print(f"{pink} ")
+time.sleep(3.0)
 print("""
-KHOB BARAYE UPDATE IN COD RO BEZAN GHABLESH TERMUX BBAND BAD DOBARE BIYA 
-IN COD 👉🕯️
-                   cd $HOME && rm -rf Filtering-Rubika && git clone https://github.com/C4ssif3r/Filtering-Rubika
-HAMASHO BEZAN""")
+  update =
+
+$       cd $HOME
+
+$       rm -rf Filtering-Rubika
+
+$       git clone https://gitHub.com/C4ssif3r/Filtering-Rubika
+
+""")
 print(f"{red} ")
+
+
 if up == "2":
 
-	input("""
-KHOB BARAYE UPDATE IN COD RO BEZAN GHABLESH TERMUX BBAND BAD DOBARE BIYA
-IN COD 👉🕯️
-                   cd $HOME && rm -rf Filtering-Rubika && git clone https://github.com/>
-HAMASHO BEZAN    HALA 2 BEZAN COD BGIR >>> """)
-print("")
-
-print("code ==>     ¥ttk_18963  ")
+        print (Fore.YELLOW + "code for Channel ≠> (/4.2.7.4.1.5.2.5./{¥\n}]/|~yttk-17926")
 
 
-print("")
-time.sleep(0.2)
+if up != "2":
+        print("error - lotfan dorost vared konid")
+
+
+
+if up == "3":
+        print ("   code group == (/Fahashi.(4.2.5.8]) ");
+
+
+
+if up != "3":
+        print("error - lotfan dorost vared konid")
+
+
+
+if up == "4":
+        print (Fore.RED + "code for account ≠> http://-5.3.5.8/f////55550/g&h3/5.2.5.7.2.5.7.8.3.2.5.7.)")
+
+
+
+if up != "4":
+        print("error - lotfan dorost vared konid")
+
+
+time.sleep(0.6)
 print(f"{green}" )
-print (""" 
+print ("""
 Kanal haye rubikam =
+
 @Cassif3r - @Hi_cassifer
+
 @Hell_RaP - @HaNi_Poot
  """)
+print ("""
+link gap rubikam ==>>> rubika.ir/joing/BCJHJHHD0MNIIVRKYYGTJTQJRNPBIIJJ
+""")
+print (Fore.WHITE + " ")
 time.sleep(3.0)
 print (" Bye Bye My Friend :)💔")
-print ("⛔ Tavajo Alan Script Pak Shode -!-💔 Baraye Ejra Mojadad abzar ❗Danlod❗ Kon ")
-print (" Termux To bband VA Dastor zir to vared Kon❗❗❗")
 time.sleep(3.0)
-print (" in Ro Bezan Baraye Danlod git clone https://github.com/C4ssif3r/Filtering")
 print (f"{red} ")
-print(" KHETLI MOHEM -!- 🕯️ BARAYE IN KE YEKI RO FILTER KONID IN 👉   /    👈  RO  BARASH BEFRESTIN  BADEZH BLAK KONIN CODE RO 1 BAR BISHTAR NAZANIN 🎶")
+print(""" KHETLI MOHEM -!-
+ BARAYE IN KE YEKI RO FILTER KONID
+ IN 👉   /    👈
+RO  BARASH BEFRESTIN
+ BADEZH BLAK KONIN
+CODE RO 1 BAR BEZANIN
+""")
 os.system("cd $HOME")
+os.system("rm -rf /data/data/com.termux/files/home/Filtering-Rubika && git clone https://github.com/C4ssif3r/Filtering-Rubika")
