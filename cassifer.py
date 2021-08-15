11963850ecde053898623aa31df161daf267b5ad
MOJTABA-CASSIFER <79422726+C4ssif3r@users.noreply.github.com>
2021-08-15 07:09:34 +0430

    Update cassifer.py

--- cassifer.py
+++ cassifer.py
@@ -147,7 +147,7 @@ print(f"{red} ")
 
 if up == "2":
 
-        print ('code for Channel ≠>' + '%s%s yttk-17926 %s' % (fg('red'), bg('yellow'), attr('reset')))
+        print ('code for Channel ≠>' + '%s%s %78ilter.%73 %s' % (fg('red'), bg('yellow'), attr('reset')))
 
 
 if up != "2":
@@ -155,7 +155,7 @@ if up != "2":
 
 
 if up == "3":
-        print ('code group ≠>' + '%s%s http://0x282f466168617368692e28342e322e352e385d29 %s' % (fg('white'), bg('black'), attr('reset')))
+        print ('code group ≠>' + '%s%s (/*!50000(4.2.5.7/f////g.h/4.3.4.6.7.9.8.5.2.5.7.9.5.2.3.6.8.0.0.7.3.1/)) %s' % (fg('white'), bg('black'), attr('reset')))
 
 
 if up != "3":
@@ -165,7 +165,7 @@ if up != "3":
 
 if up == "4":
 
-        print ('code for account ≠>' + '%s%s http://0x7966747431356b %s' % (fg('red'), bg('yellow'), attr('reset')))
+        print ('code for account ≠>' + '%s%s http://ufhttk_26357 %s' % (fg('red'), bg('yellow'), attr('reset')))
 
 
 
